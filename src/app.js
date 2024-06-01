9af351dbd5f0d5c91251f8ae78781ddb7f23b65c
LuisRe135 <64992020+LuisRe135@users.noreply.github.com>
2024-06-01 20:26:57 +0000

    excuses

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+
+  let action = ["ate", "peed", "crushed", "broke"];
+
+  let what = ["my homework", "my phone", "the car"];
+
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generateExcuse(who, action, what, when) {
+    let excuse1 =
+      who[Math.floor(Math.random() * who.length)] +
+      " " +
+      action[Math.floor(Math.random() * action.length)] +
+      " " +
+      what[Math.floor(Math.random() * what.length)] +
+      " " +
+      when[Math.floor(Math.random() * when.length)];
+    return excuse1;
+  }
+  document.getElementById("excuse").innerHTML = generateExcuse(
+    who,
+    action,
+    what,
+    when
+  );
+
+  // console.log(generateExcuse(who, action, what, when));
 };
